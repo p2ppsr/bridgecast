c99159482fb2d97f22c7b44579ca1e5cc72c4664
Ty Everett <ty@tyweb.us>
2021-11-11 14:31:14 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const parapet = require('parapet-js')
+const boomerang = require('@cwi/boomerang')
+
+/**
+ * Notify Bridgeport bridges about new transactions
+ *
+ * @param {Object} obj All parameters are provided in an object
+ * @param {Array<String>} obj.bridges The array of Bridge IDs to notify
+ * @param {Object} obj.tx An Everett-style transaction envelope
+ *
+ * @returns {Promise<Array<String>>} Bridge IDs that accepted the transaction
+ */
+const bridgecast = async ({ bridges = [], tx } = {}) => {
+  const broadcastURLs = {}
+
+  // For each bridge that accepted the tx, look up other BHRP nodes
+  for (const bridge of bridges) {
+    const nodes = await parapet({
+      bridge: '1TW5ogeDZyvq5q7tEjpNcBmJWsmQk7AeQ', // BHRP
+      request: {
+        type: 'json-query',
+        query: {
+          v: 3,
+          q: {
+            collection: 'bridges',
+            find: {
+              bridge
+            },
+            project: {
+              URL: 1
+            }
+          }
+        }
+      }
+    })
+    for (const node of nodes) {
+      broadcastURLs[`${node.URL}/processTransaction`] = 1
+    }
+  }
+
+  const acceptingBridges = {}
+
+  // Send the transaction to each of the bridges
+  for (const broadcastURL of Object.keys(broadcastURLs)) {
+    try {
+      const result = await boomerang(
+        'POST',
+        broadcastURL,
+        {
+          ...tx,
+          bridges
+        }
+      )
+      result.acceptingBridges.forEach(x => (acceptingBridges[x] = 1))
+    } catch (e) { /* not our problem */ }
+  }
+  return Object.keys(acceptingBridges)
+}
+
+module.exports = bridgecast
